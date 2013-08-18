56e4a7a37001dce9759bd09cc1b806ccd368a10b
BB <bb.bartosz@gmail.com>
2013-08-18 21:28:29 +0200

    working

--- practicalUnitTesting/src/main/java/pl/bb/practicalUnitTesting/bookingSystem/SimpleBookingSystem.java
+++ practicalUnitTesting/src/main/java/pl/bb/practicalUnitTesting/bookingSystem/SimpleBookingSystem.java
@@ -1,22 +1,36 @@
 package pl.bb.practicalUnitTesting.bookingSystem;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SimpleBookingSystem {
 	private final boolean[] bookings = new boolean[24];
 
 	public void makeReservation(int hourFrom, int hourTo) {
+		checkIfAnyHourInRangeBooked(hourFrom, hourTo);
+		Arrays.fill(bookings, hourFrom, hourTo, true);
+	}
+
+	private void checkIfAnyHourInRangeBooked(int hourFrom, int hourTo) {
 		for (int i = hourFrom; i < hourTo; i++) {
-			if (bookings[i]) {
+			if (isBooked(i)) {
 				throw new AlreadyBookedException();
 			}
 		}
-
-		Arrays.fill(bookings, hourFrom, hourTo, true);
 	}
 
 	public boolean isBooked(int i) {
 		return bookings[i];
 	}
 
+	public Integer[] getBookedHours() {
+		Set<Integer> ret = new HashSet<Integer>();
+		for (int i = 0; i < 24; i++) {
+			if (isBooked(i)) {
+				ret.add(i);
+			}
+		}
+		return ret.toArray(new Integer[0]);
+	}
 }
--- practicalUnitTesting/src/test/java/pl/bb/practicalUnitTesting/bookingSystem/SimpleBookingSystemTest.java
+++ practicalUnitTesting/src/test/java/pl/bb/practicalUnitTesting/bookingSystem/SimpleBookingSystemTest.java
@@ -1,7 +1,7 @@
 package pl.bb.practicalUnitTesting.bookingSystem;
 
-import junit.framework.Assert;
-
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 @Test
@@ -21,5 +21,22 @@ public class SimpleBookingSystemTest {
 		SimpleBookingSystem booking = new SimpleBookingSystem();
 		booking.makeReservation(12, 13);
 		booking.makeReservation(12, 13);
+	}
+
+	@Test(dataProvider = "bookedHoursDataProvider")
+	public void bookedHoursTest(Integer[] hours) {
+		SimpleBookingSystem booking = new SimpleBookingSystem();
+
+		for (int i = 0; i < hours.length; i++) {
+			booking.makeReservation(hours[i], hours[i] + 1);
+		}
+
+		Integer[] bookedHours = booking.getBookedHours();
+		Assert.assertEqualsNoOrder(bookedHours, hours);
+	}
+
+	@DataProvider
+	private Object[][] bookedHoursDataProvider() {
+		return new Object[][] { { new Integer[] { 1, 2 } }, { new Integer[] { 12, 11, 10 } } };
 	}
 }
